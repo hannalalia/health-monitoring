3dcd91361a82d81dcf8743b334d5bb26a3ae6eef
Hanna <you@example.com>
2021-06-18 18:08:20 +0800

    change remaining placeholder links to route links

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -10,8 +10,8 @@ function TopNav(){
         <Nav className="mr-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/Health">Health Status</Nav.Link>
-            <Nav.Link href="#link2">Exposure Reports</Nav.Link>
-            <Nav.Link href="#link3">RT PCR Tests</Nav.Link>
+            <Nav.Link href="/Reports">Exposure Reports</Nav.Link>
+            <Nav.Link href="/Tests">RT PCR Tests</Nav.Link>
             <NavDropdown title="Employee Name" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">Logout</NavDropdown.Item>
