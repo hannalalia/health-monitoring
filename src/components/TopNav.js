4b33e35f1fd33afea4b80759c3ebf10375401b44
Hanna <you@example.com>
2021-06-18 18:06:25 +0800

    change placeholder links to route links

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -4,12 +4,12 @@ import {Navbar,NavDropdown, Nav} from 'react-bootstrap';
 function TopNav(){
     return(
     <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">NNC</Navbar.Brand>
+        <Navbar.Brand href="/">NNC</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link1">Health Status</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/Health">Health Status</Nav.Link>
             <Nav.Link href="#link2">Exposure Reports</Nav.Link>
             <Nav.Link href="#link3">RT PCR Tests</Nav.Link>
             <NavDropdown title="Employee Name" id="basic-nav-dropdown">
