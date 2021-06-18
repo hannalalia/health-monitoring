315885aee23e6c6ac76ae2e9dc4fe1ff3ecf2c4d
Hanna <you@example.com>
2021-06-18 18:03:11 +0800

    Add routes for HealthStatus and TopNav components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dashboard from './components/Dashboard';
 import TopNav from './components/TopNav';
+import HealthStatus from './components/HealthStatus';
 import { Container } from 'react-bootstrap';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Container className="mb-2">
-        <TopNav></TopNav>
-        <Dashboard></Dashboard>
+        <Router>
+          <TopNav></TopNav>
+          <Switch>
+            <Route exact path="/">
+              <Dashboard></Dashboard>
+            </Route>
+            <Route exact path="/Health">
+              <HealthStatus></HealthStatus>
+            </Route>
+          </Switch>
+        </Router>      
       </Container>
     </div>
   );
