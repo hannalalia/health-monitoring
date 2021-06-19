a31e2233aa6b10863fe49c2f4007693bf088a7f7
Hanna <you@example.com>
2021-06-19 16:40:22 +0800

    fix mobile responsiveness of search filter

--- src/components/GlobalFilter.js
+++ src/components/GlobalFilter.js
@@ -3,11 +3,10 @@ import {Form} from 'react-bootstrap'
 
 function GlobalFilter({filter, setFilter}) {
     return ( 
-        <span>
-            <Form.Control className="w-25" type = "text" placeholder="Search" value={filter||''} 
+        <Form.Control className="my-2 col-12 col-sm-3" type = "text" placeholder="Search" value={filter||''} 
             onChange={e=> setFilter(e.target.value)}>
-            </Form.Control>
-        </span>
+        </Form.Control>
+
     );
 }
  
