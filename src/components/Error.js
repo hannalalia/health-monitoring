64b9e914c2a3589b9ffd03ed61981e5b9d54bb7a
Hanna <you@example.com>
2021-06-20 20:57:20 +0800

    remove grey background of error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,11 +3,9 @@ import {Container, Jumbotron} from 'react-bootstrap'
 
 function Error(){
     return ( 
-        <Container fluid>
-            <Jumbotron className="text-center w-75 mx-auto mt-5">
+        <Container fluid className="text-center w-75 mx-auto mt-5">
                 <img src="assets/404.svg" className="img-fluid"></img>
                 <h1>Page Not Found</h1>
-            </Jumbotron>
         </Container>
      );
 }
