7b6894139da9de0226137ec0149d889e6d7d480e
Hanna <you@example.com>
2021-06-20 20:52:12 +0800

    redesign error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,15 @@
 import React from "react";
+import {Container, Jumbotron} from 'react-bootstrap'
 
 function Error(){
-    return ( "page not found" );
+    return ( 
+        <Container fluid>
+            <Jumbotron className="text-center w-75 mx-auto mt-5">
+                <img src="assets/404.svg" className="img-fluid"></img>
+                <h1>Page Not Found</h1>
+            </Jumbotron>
+        </Container>
+     );
 }
  
 export default Error;
