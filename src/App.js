8c4f39b3398f24e89179a6770fd0d3bae0dbd51f
Hanna <you@example.com>
2021-06-21 23:57:43 +0800

    remove navbar on login page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Dashboard from './components/Dashboard';
-import TopNav from './components/TopNav';
-import HealthStatus from './components/HealthStatus';
+import { Fragment } from 'react';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ExposureReports from './components/ExposureReports';
@@ -10,32 +8,36 @@ import RTPCRTest from './components/RTPCRTest';
 import Error from './components/Error';
 import TravelReports from './components/TravelReports';
 import Login from './components/Login';
-
+import Dashboard from './components/Dashboard';
+import TopNav from './components/TopNav';
+import HealthStatus from './components/HealthStatus';
 function App() {
   return (
     <div className="App">
       <Container className="mb-2">
-      <TopNav></TopNav>
-        <Router>
-          <Switch>
-            <Route exact path="/">
-              <Dashboard></Dashboard>
-            </Route>
-            <Route exact path="/Health">
-              <HealthStatus></HealthStatus>
-            </Route>
-            <Route exact path="/Travel">
-              <TravelReports></TravelReports>
-            </Route>
-            <Route exact path="/Exposure">
-              <ExposureReports></ExposureReports>
-            </Route>
-            <Route exact path="/Tests">
-              <RTPCRTest></RTPCRTest>
-            </Route>
+        <Router>   
+          <Switch>   
             <Route exact path="/Login">
               <Login></Login>
-            </Route>
+            </Route>    
+            <div>
+              <TopNav></TopNav>
+              <Route exact path="/">
+                <Dashboard></Dashboard>
+              </Route>
+              <Route exact path="/Health">
+                <HealthStatus></HealthStatus>
+              </Route>
+              <Route exact path="/Travel">
+                <TravelReports></TravelReports>
+              </Route>
+              <Route exact path="/Exposure">
+                <ExposureReports></ExposureReports>
+              </Route>
+              <Route exact path="/Tests">
+                <RTPCRTest></RTPCRTest>
+              </Route>                  
+            </div>
             <Route>
               <Error></Error>
             </Route>
