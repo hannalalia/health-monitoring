12c83703327d6097fc33ea6de28b7841f31f824b
Hanna <you@example.com>
2021-06-22 00:41:49 +0800

    remove error route

--- src/App.js
+++ src/App.js
@@ -40,11 +40,11 @@ function App() {
               </Route>    
               <Route exact path="/Reset">
                 <ResetPassword></ResetPassword>
-              </Route>   
-              <Route>
-                <Error></Error>
-              </Route>           
+              </Route>              
             </div>
+            <Route>
+              <Error></Error>
+            </Route>
           </Switch>
         </Router>     
       </Container>
